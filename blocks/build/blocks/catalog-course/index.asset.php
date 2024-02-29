9eb4c532bb7b6e267bb39554b5e2aebe5aa1b873
Holly Pekrul <hpekrul@my.wctc.edu>
2024-02-29 16:28:05 -0600

    Ignored build folder

--- blocks/build/blocks/catalog-course/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c127482da25505b04bbd');
--- blocks/build/blocks/catalog-course/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '3aed22c65e10eb863822');
