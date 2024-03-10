ea19ee12b7e1c8eaa9f9165ae4a594f895ec8247
Holly Pekrul <hpekrul@my.wctc.edu>
2024-03-10 13:00:41 -0500

    added code to make HERA event banner pattern

--- blocks/patterns/hera-feature-banner.php
+++ blocks/patterns/hera-feature-banner.php
@@ -8,6 +8,36 @@
  * https://developer.wordpress.org/themes/features/block-patterns/#registering-block-patterns
  */
 ?>
-<!-- wp:heading -->
-<h2 class="wp-block-heading">Some Heading</h2>
-<!-- /wp:heading -->
+<!-- wp:group {"layout":{"type":"constrained"},"metadata":{"name":"HERA Event Banner"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"color":{"background":"#a2c9637d"}},"layout":{"type":"constrained"},"metadata":{"name":"Hera Event Banner"}} -->
+	<div class="wp-block-group has-background" style="background-color:#a2c9637d"><!-- wp:columns -->
+		<div class="wp-block-columns"><!-- wp:column {"width":""} -->
+			<div class="wp-block-column"><!-- wp:buttons -->
+				<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"white","style":{"border":{"radius":"3px"},"color":{"text":"#000000"},"elements":{"link":{"color":{"text":"#000000"}}}},"className":"is-style-fill","fontSize":"small"} -->
+					<div class="wp-block-button has-custom-font-size is-style-fill has-small-font-size"><a class="wp-block-button__link has-white-background-color has-text-color has-background has-link-color wp-element-button" style="border-radius:3px;color:#000000">Upcoming Event</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons -->
+
+				<!-- wp:heading {"level":3} -->
+				<h3 class="wp-block-heading">Title of Event</h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph -->
+				<p>Brief paragraph about event. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:buttons -->
+				<div class="wp-block-buttons"><!-- wp:button {"style":{"color":{"text":"#000000","background":"#ffffff00"},"elements":{"link":{"color":{"text":"#000000"}}}},"className":"is-style-outline"} -->
+					<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-text-color has-background has-link-color wp-element-button" style="color:#000000;background-color:#ffffff00">Learn More About Event</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column -->
+			<div class="wp-block-column"><!-- wp:image {"id":579,"width":"371px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
+				<figure class="wp-block-image size-full is-resized"><img src="http://localhost/wp-content/uploads/2022/01/characters-for-book-1.jpg" alt="" class="wp-image-579" style="width:371px;height:auto"/></figure>
+				<!-- /wp:image --></div>
+			<!-- /wp:column --></div>
+		<!-- /wp:columns --></div>
+	<!-- /wp:group --></div>
+<!-- /wp:group -->
