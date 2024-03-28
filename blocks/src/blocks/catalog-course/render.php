4d78c5c6317ce31a91e41f2407ceeeb8997d1b7c
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-03-28 15:26:39 -0500

    Added advanced filter search function

--- blocks/src/blocks/catalog-course/render.php
+++ blocks/src/blocks/catalog-course/render.php
@@ -9,12 +9,82 @@
  * @var WP_Block $block All the
  */
 
+// get search keywords
 $searchKey = $_GET['searchKey'] ?? '';
-$searchKey = strip_tags($searchKey);
+$orderBy = $_GET['orderby'] ?? 'post_title';
+$dir = $_GET['order'] ?? 'ASC';
 
-if(!(isset($_GET['orderby']) && isset($_GET['order']))) {
-	$_GET['orderby'] = 'post_title';
-	$_GET['order'] = 'ASC';
+// validate input
+$searchKey = strip_tags($searchKey);
+$orderBy = strip_tags($orderBy);
+$dir = strip_tags($dir);
+
+if($_GET['orderby'] === 'title') {
+	$orderBy = 'post_title';
+}
+
+// get all the options of ACF fields
+$institutionField = get_field_object('field_65e107c02dd8e');
+$topicField = get_field_object('field_65e108d52dd93');
+$formatField = get_field_object('field_65e108062dd8f');
+$audienceField = get_field_object('field_65e108b82dd92');
+$durationField = get_field_object('field_65e108f62dd94');
+
+// get filter inputs
+$topicInput = $_GET['course-topic'] ?? '';
+$institutionInput = $_GET['course-institution'] ?? '';
+$audienceInput = $_GET['course-audience'] ?? '';
+$durationInput = $_GET['course-duration'] ?? '';
+$formatInput = $_GET['course-format'] ?? '';
+
+if($topicInput === '') {
+	$topicMeta = '';
+} else {
+	$topicMeta = array(
+		'key' => 'hera_course_topic',
+		'value' => $topicInput,
+		'compare' => 'LIKE'
+	);
+}
+
+if($institutionInput === '') {
+	$institutionMeta = '';
+} else {
+	$institutionMeta = array(
+		'key' => 'hera_course_institution',
+		'value' => $institutionInput,
+		'compare' => '='
+	);
+}
+
+if($audienceInput === '') {
+	$audienceMeta = '';
+} else {
+	$audienceMeta = array(
+		'key' => 'hera_course_audience',
+		'value' => $audienceInput,
+		'compare' => '='
+	);
+}
+
+if($durationInput === '') {
+	$durationMeta = '';
+} else {
+	$durationMeta = array(
+		'key' => 'hera_course_duration',
+		'value' => $durationInput,
+		'compare' => '='
+	);
+}
+
+if($formatInput === '') {
+	$formatMeta = '';
+} else {
+	$formatMeta = array(
+		'key' => 'hera_course_format',
+		'value' => $formatInput,
+		'compare' => '='
+	);
 }
 
 $query = new WP_Query([
@@ -26,12 +96,19 @@ $query = new WP_Query([
 			'key' => 'hera_course_institution',
 			'compare' => 'EXISTS'
 		],
+		array(
+			'relation' => 'AND',
+			$topicMeta,
+			$institutionMeta,
+			$audienceMeta,
+			$durationMeta,
+			$formatMeta
+		)
 	],
 	'orderby' => [
-		$_GET['orderby'] => $_GET['order']
+		$orderBy => $dir
 	]
-])
-
+]);
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
 	<section class="search-section">
@@ -39,6 +116,85 @@ $query = new WP_Query([
 		<form method="get">
 			<div class="input-group mb-3">
 				<input id="searchKey" value="<?= $searchKey ?>" name="searchKey" type="text" class="form-control" placeholder="e.g. marketing" aria-label="Search keywords" aria-describedby="A search bar for course name">
+			</div>
+
+			<!-- advanced search -->
+			<div class="accordion mb-3" id="filterAccordion">
+				<div class="accordion-item">
+					<h2 class="accordion-header">
+						<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFilters" aria-expanded="true" aria-controls="collapseFilters">
+							Advanced Search
+						</button>
+					</h2>
+					<div id="collapseFilters" class="accordion-collapse collapse" data-bs-parent="#filterAccordion">
+						<div class="accordion-body">
+							<div class="row">
+								<div class="col-md-6 mb-3">
+									<label for="course-topic">Topic</label>
+									<select class="form-select" name="course-topic" id="course-topic">
+										<option value="">Any</option>
+										<?php
+										foreach($topicField['choices'] as $value => $label):
+										?>
+										<option value="<?= $value ?>"><?= $label ?></option>
+										<?php endforeach; ?>
+									</select>
+								</div>
+								<div class="col-md-6 mb-3">
+									<label for="course-institution">Institution</label>
+									<select class="form-select" name="course-institution" id="course-institution">
+										<option value="">Any</option>
+										<?php
+										foreach($institutionField['choices'] as $value => $label):
+											?>
+											<option value="<?= $value ?>"><?= $label ?></option>
+										<?php endforeach; ?>
+									</select>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-6 mb-3">
+									<label>Audience</label>
+									<?php
+									foreach($audienceField['choices'] as $value => $label):
+										?>
+										<div>
+											<input type="checkbox" id="<?= $value ?>" value="<?= $value ?>" name="course-audience" />
+											<label for="<?= $value ?>"><?= $label ?></label>
+										</div>
+									<?php endforeach; ?>
+								</div>
+								<div class="col-md-6 mb-3">
+									<label>Duration</label>
+									<?php
+									foreach($durationField['choices'] as $value => $label):
+										?>
+										<div>
+											<input type="checkbox" id="<?= $value ?>" value="<?= $value ?>" name="course-duration" />
+											<label for="<?= $value ?>"><?= $label ?></label>
+										</div>
+									<?php endforeach; ?>
+								</div>
+							</div>
+							<div class="row">
+								<label>Format</label>
+								<?php
+								foreach($formatField['choices'] as $value => $label):
+									?>
+									<div class="course-format-item">
+										<input type="checkbox" id="<?= $value ?>" value="<?= $value ?>" name="course-format" />
+										<div>
+										  <span class="format-option text-center">
+											  <span class="icon <?= $value ?>"></span><br/>
+											  <?= $label ?>
+										  </span>
+										</div>
+									</div>
+								<?php endforeach; ?>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 			<button type="submit" class="btn btn-primary btn-sm mb-3">Search</button>
 		</form>
@@ -49,29 +205,29 @@ $query = new WP_Query([
 				class="form-select"
 				id="sortBy"
 				name="sortBy"
-				onchange="document.location.href='?searchKey=<?= $searchKey ?>&'+this.options[this.selectedIndex].value;"
+				onchange="document.location.href=this.options[this.selectedIndex].value;"
 			>
 				<option
 					<?php
 					if( isset($_GET["orderby"]) &&
-						trim($_GET["orderby"]) == 'post_title' &&
+						trim($_GET["orderby"]) == 'title' &&
 						isset($_GET["order"]) &&
 						trim($_GET["order"]) == 'ASC' ) {
 						echo 'selected';
 					}
 					?>
-					value="orderby=post_title&order=ASC"
+					value="?searchKey=<?= $searchKey ?>&orderby=title&order=ASC"
 				>Course Name (A-Z)</option>
 				<option
 					<?php
 					if( isset($_GET["orderby"]) &&
-						trim($_GET["orderby"]) == 'post_title' &&
+						trim($_GET["orderby"]) == 'title' &&
 						isset($_GET["order"]) &&
 						trim($_GET["order"]) == 'DESC' ) {
 						echo 'selected';
 					}
 					?>
-					value="orderby=post_title&order=DESC"
+					value="?searchKey=<?= $searchKey ?>&orderby=title&order=DESC"
 				>Course Name (Z-A)</option>
 				<option
 					<?php
@@ -82,7 +238,7 @@ $query = new WP_Query([
 						echo 'selected';
 					}
 					?>
-					value="orderby=institution&order=ASC"
+					value="?searchKey=<?= $searchKey ?>&orderby=institution&order=ASC"
 				>Institution (A-Z)</option>
 				<option
 					<?php
@@ -93,7 +249,7 @@ $query = new WP_Query([
 						echo 'selected';
 					}
 					?>
-					value="orderby=institution&order=DESC"
+					value="?searchKey=<?= $searchKey ?>&orderby=institution&order=DESC"
 				>Institution (Z-A)</option>
 			</select>
 			<div class="btn-group">
