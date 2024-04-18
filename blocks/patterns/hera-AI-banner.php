eeb664346080614b434b1ce07fe0592c2ff62db7
Holly Pekrul <hpekrul@my.wctc.edu>
2024-04-18 09:04:35 -0500

    updated patterns to have rows and container

--- blocks/patterns/hera-AI-banner.php
+++ blocks/patterns/hera-AI-banner.php
@@ -8,36 +8,34 @@
  * https://developer.wordpress.org/themes/features/block-patterns/#registering-block-patterns
  */
 ?>
-<!-- wp:group {"style":{"color":{"background":"#9bdbf3"}},"className":"p-5 alignfull","layout":{"type":"constrained"},"metadata":{"name":"HERA AI Banner"}} -->
-<div class="wp-block-group p-5 alignfull has-background" style="background-color:#9bdbf3"><!-- wp:columns -->
-	<div class="wp-block-columns"><!-- wp:column {"className":"p-5"} -->
-		<div class="wp-block-column p-5"><!-- wp:heading {"level":3,"className":"pb-2"} -->
-			<h3 class="wp-block-heading pb-2">Looking to expand your knowledge of Artificial Intelligence (AI)?</h3>
-			<!-- /wp:heading -->
+<!-- wp:group {"style":{"color":{"background":"#9bdbf3"}},"className":"alignfull","layout":{"type":"flex","flexWrap":"nowrap"},"metadata":{"name":"HERA-AI-BANNER"}} -->
+<div class="wp-block-group alignfull has-background" style="background-color:#9bdbf3"><!-- wp:group {"className":"container m-auto","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group container m-auto"><!-- wp:columns {"className":"py-5"} -->
+		<div class="wp-block-columns py-5"><!-- wp:column {"width":"66.66%"} -->
+			<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:heading {"level":3,"className":"pb-2"} -->
+				<h3 class="wp-block-heading pb-2">Looking to expand your knowledge of Artificial Intelligence (AI)?</h3>
+				<!-- /wp:heading -->
 
-			<!-- wp:heading {"level":4,"className":"pb-2"} -->
-			<h4 class="wp-block-heading pb-2">Earn A Badge On AI</h4>
-			<!-- /wp:heading -->
+				<!-- wp:heading {"level":4,"className":"pb-2"} -->
+				<h4 class="wp-block-heading pb-2">Earn A Badge On AI</h4>
+				<!-- /wp:heading -->
 
-			<!-- wp:paragraph -->
-			<p>For the first time, HERA is offering a Micro credential AI course that will provide you with knowledge an earn you a badge.</p>
-			<!-- /wp:paragraph -->
+				<!-- wp:paragraph -->
+				<p>For the first time, HERA is offering a Micro credential AI course that will provide you with knowledge an earn you a badge.</p>
+				<!-- /wp:paragraph -->
 
-			<!-- wp:buttons -->
-			<div class="wp-block-buttons"><!-- wp:button {"className":"pt-1 yellow-button is-style-outline"} -->
-				<div class="wp-block-button pt-1 yellow-button is-style-outline"><a class="wp-block-button__link wp-element-button">View Course </a></div>
-				<!-- /wp:button --></div>
-			<!-- /wp:buttons --></div>
-		<!-- /wp:column -->
+				<!-- wp:buttons -->
+				<div class="wp-block-buttons"><!-- wp:button {"className":"pt-1 yellow-button is-style-outline"} -->
+					<div class="wp-block-button pt-1 yellow-button is-style-outline"><a class="wp-block-button__link wp-element-button">View Course </a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:column -->
 
-		<!-- wp:column {"className":"p-5"} -->
-		<div class="wp-block-column p-5"><!-- wp:spacer {"height":"39px"} -->
-			<div style="height:39px" aria-hidden="true" class="wp-block-spacer"></div>
-			<!-- /wp:spacer -->
-
-			<!-- wp:image {"align":"center","id":1380,"sizeSlug":"full","linkDestination":"none"} -->
-			<figure class="wp-block-image aligncenter size-full"><img src="https://place-hold.it/200" alt="" class="wp-image-1380"/></figure>
-			<!-- /wp:image --></div>
-		<!-- /wp:column --></div>
-	<!-- /wp:columns --></div>
+			<!-- wp:column {"width":"33.33%"} -->
+			<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"align":"center","id":1380,"sizeSlug":"full","linkDestination":"none"} -->
+				<figure class="wp-block-image aligncenter size-full"><img src="https://place-hold.it/200" alt="" class="wp-image-1380"/></figure>
+				<!-- /wp:image --></div>
+			<!-- /wp:column --></div>
+		<!-- /wp:columns --></div>
+	<!-- /wp:group --></div>
 <!-- /wp:group -->
--- blocks/patterns/hera-feature-banner.php
+++ blocks/patterns/hera-feature-banner.php
@@ -8,15 +8,11 @@
  * https://developer.wordpress.org/themes/features/block-patterns/#registering-block-patterns
  */
 ?>
-<!-- wp:group {"className":"hera-banner-pattern","layout":{"type":"constrained"},"metadata":{"name":"HERA Event Banner"}} -->
-<div class="wp-block-group hera-banner-pattern"><!-- wp:group {"style":{"color":{"background":"#a2c9637d"}},"className":"p-5 alignfull","layout":{"type":"constrained"},"metadata":{"name":"Hera Event Banner"}} -->
-	<div class="wp-block-group p-5 alignfull has-background" style="background-color:#a2c9637d"><!-- wp:columns -->
-		<div class="wp-block-columns"><!-- wp:column {"width":"","className":"p-5"} -->
-			<div class="wp-block-column p-5"><!-- wp:spacer -->
-				<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
-				<!-- /wp:spacer -->
-
-				<!-- wp:buttons -->
+<!-- wp:group {"style":{"color":{"background":"#a2c9637d"}},"className":"alignfull","layout":{"type":"flex","flexWrap":"nowrap"},"metadata":{"name":"HERA-EVENT-BANNER"}} -->
+<div class="wp-block-group alignfull has-background" style="background-color:#a2c9637d"><!-- wp:group {"className":"container m-auto","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group container m-auto"><!-- wp:columns {"className":"py-5"} -->
+		<div class="wp-block-columns py-5"><!-- wp:column {"width":"66.66%"} -->
+			<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:buttons -->
 				<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"white","style":{"border":{"radius":"3px"},"color":{"text":"#000000"},"elements":{"link":{"color":{"text":"#000000"}}}},"className":"is-style-fill pb-2","fontSize":"small"} -->
 					<div class="wp-block-button has-custom-font-size is-style-fill pb-2 has-small-font-size"><a class="wp-block-button__link has-white-background-color has-text-color has-background has-link-color wp-element-button" style="border-radius:3px;color:#000000">Upcoming Event</a></div>
 					<!-- /wp:button --></div>
@@ -32,17 +28,13 @@
 
 				<!-- wp:buttons {"className":"pt-2"} -->
 				<div class="wp-block-buttons pt-2"><!-- wp:button {"style":{"color":{"text":"#000000","background":"#ffffff00"},"elements":{"link":{"color":{"text":"#000000"}}}},"className":"is-style-outline yellow-button"} -->
-					<div class="wp-block-button is-style-outline yellow-button"><a class="wp-block-button__link has-text-color has-background has-link-color wp-element-button" style="color:#000000;background-color:#ffffff00">Learn More About Event</a></div>
+					<div class="wp-block-button is-style-outline yellow-button"><a class="wp-block-button__link has-text-color has-background has-link-color wp-element-button" style="color:#000000;background-color:#ffffff00">Learn More</a></div>
 					<!-- /wp:button --></div>
 				<!-- /wp:buttons --></div>
 			<!-- /wp:column -->
 
-			<!-- wp:column {"className":"p-5"} -->
-			<div class="wp-block-column p-5"><!-- wp:spacer {"height":"60px"} -->
-				<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
-				<!-- /wp:spacer -->
-
-				<!-- wp:image {"align":"center","id":1320,"width":"473px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
+			<!-- wp:column {"width":"33.33%"} -->
+			<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"align":"center","id":1320,"width":"473px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
 				<figure class="wp-block-image aligncenter size-full is-resized"><img src="https://place-hold.it/200" alt="" class="wp-image-1320" style="width:473px;height:auto"/></figure>
 				<!-- /wp:image --></div>
 			<!-- /wp:column --></div>
